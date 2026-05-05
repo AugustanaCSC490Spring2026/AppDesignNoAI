6ea07431956a77913e21ef8742dc858f488e26fa
aidanhancock22 <aidanhancock22@augustana.edu>
2026-05-05 17:40:11 -0500

    updated results

--- design_app/src/App.tsx
+++ design_app/src/App.tsx
@@ -54,7 +54,7 @@ function App() {
     "3 Repeated Letters",
     "4+ Repeated Letters",
   ];
-  const id = 3;
+  const id = 10;
 
   async function handleFinishedGrouping(groupedResults: string[][]) {
     const score = handleScore(groupedResults, correct);
